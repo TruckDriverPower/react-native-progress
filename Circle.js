29948a2518a94a8e4f9d8e22a1a283551343d963
Jose Gomez <jose4gga@gmail.com>
2017-07-06 08:40:31 -0400

    Children inside of the circle

--- Circle.js
+++ Circle.js
@@ -1,15 +1,9 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import {
-  Animated,
-  ART,
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { Animated, ART, StyleSheet, Text, View } from "react-native";
 
-import Arc from './Shapes/Arc';
-import withAnimation from './withAnimation';
+import Arc from "./Shapes/Arc";
+import withAnimation from "./withAnimation";
 
 const CIRCLE = Math.PI * 2;
 
@@ -18,9 +12,9 @@ const AnimatedArc = Animated.createAnimatedComponent(Arc);
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: 'transparent',
-    overflow: 'hidden',
-  },
+    backgroundColor: "transparent",
+    overflow: "hidden"
+  }
 });
 
 export class ProgressCircle extends Component {
@@ -30,31 +24,28 @@ export class ProgressCircle extends Component {
     borderWidth: PropTypes.number,
     color: PropTypes.string,
     children: React.PropTypes.node,
-    direction: PropTypes.oneOf(['clockwise', 'counter-clockwise']),
+    direction: PropTypes.oneOf(["clockwise", "counter-clockwise"]),
     formatText: PropTypes.func,
     indeterminate: PropTypes.bool,
-    progress: PropTypes.oneOfType([
-      PropTypes.number,
-      PropTypes.instanceOf(Animated.Value),
-    ]),
+    progress: PropTypes.oneOfType([PropTypes.number, PropTypes.instanceOf(Animated.Value)]),
     rotation: PropTypes.instanceOf(Animated.Value),
     showsText: PropTypes.bool,
     size: PropTypes.number,
     style: View.propTypes.style,
     textStyle: Text.propTypes.style,
     thickness: PropTypes.number,
-    unfilledColor: PropTypes.string,
+    unfilledColor: PropTypes.string
   };
 
   static defaultProps = {
     borderWidth: 1,
-    color: 'rgba(0, 122, 255, 1)',
-    direction: 'clockwise',
+    color: "rgba(0, 122, 255, 1)",
+    direction: "clockwise",
     formatText: progress => `${Math.round(progress * 100)}%`,
     progress: 0,
     showsText: false,
     size: 40,
-    thickness: 3,
+    thickness: 3
   };
 
   constructor(props, context) {
@@ -65,7 +56,7 @@ export class ProgressCircle extends Component {
 
   componentWillMount() {
     if (this.props.animated) {
-      this.props.progress.addListener((event) => {
+      this.props.progress.addListener(event => {
         this.progressValue = event.value;
         if (this.props.showsText || this.progressValue === 1) {
           this.forceUpdate();
@@ -75,36 +66,17 @@ export class ProgressCircle extends Component {
   }
 
   render() {
-    const {
-      animated,
-      borderColor,
-      borderWidth,
-      color,
-      children,
-      direction,
-      formatText,
-      indeterminate,
-      progress,
-      rotation,
-      showsText,
-      size,
-      style,
-      strokeCap,
-      textStyle,
-      thickness,
-      unfilledColor,
-      ...restProps
-    } = this.props;
+    const { animated, borderColor, borderWidth, color, children, direction, formatText, indeterminate, progress, rotation, showsText, size, style, strokeCap, textStyle, thickness, unfilledColor, ...restProps } = this.props;
 
     const border = borderWidth || (indeterminate ? 1 : 0);
 
-    const radius = (size / 2) - border;
+    const radius = size / 2 - border;
     const offset = {
       top: border,
-      left: border,
+      left: border
     };
     const textOffset = border + thickness;
-    const textSize = size - (textOffset * 2);
+    const textSize = size - textOffset * 2;
 
     const Surface = rotation ? AnimatedSurface : ART.Surface;
     const Shape = animated ? AnimatedArc : Arc;
@@ -117,74 +89,52 @@ export class ProgressCircle extends Component {
           width={size}
           height={size}
           style={{
-            transform: [{
-              rotate: indeterminate && rotation
-                ? rotation.interpolate({
-                  inputRange: [0, 1],
-                  outputRange: ['0deg', '360deg'],
-                })
-                : '0deg',
-            }],
+            transform: [
+              {
+                rotate:
+                  indeterminate && rotation
+                    ? rotation.interpolate({
+                        inputRange: [0, 1],
+                        outputRange: ["0deg", "360deg"]
+                      })
+                    : "0deg"
+              }
+            ]
           }}
         >
-          {unfilledColor && progressValue !== 1 ? (
-            <Shape
-              radius={radius}
-              offset={offset}
-              startAngle={angle}
-              endAngle={CIRCLE}
-              direction={direction}
-              stroke={unfilledColor}
-              strokeWidth={thickness}
-            />
-          ) : false}
-          {!indeterminate ? (
-            <Shape
-              radius={radius}
-              offset={offset}
-              startAngle={0}
-              endAngle={angle}
-              direction={direction}
-              stroke={color}
-              strokeWidth={thickness}
-            />
-          ) : false}
-          {border ? (
-            <Arc
-              radius={size / 2}
-              startAngle={0}
-              endAngle={(indeterminate ? 1.8 : 2) * Math.PI}
-              stroke={borderColor || color}
-              strokeCap={strokeCap}
-              strokeWidth={border}
-            />
-          ) : false}
+          {unfilledColor && progressValue !== 1 ? <Shape radius={radius} offset={offset} startAngle={angle} endAngle={CIRCLE} direction={direction} stroke={unfilledColor} strokeWidth={thickness} /> : false}
+          {!indeterminate ? <Shape radius={radius} offset={offset} startAngle={0} endAngle={angle} direction={direction} stroke={color} strokeWidth={thickness} /> : false}
+          {border ? <Arc radius={size / 2} startAngle={0} endAngle={(indeterminate ? 1.8 : 2) * Math.PI} stroke={borderColor || color} strokeCap={strokeCap} strokeWidth={border} /> : false}
         </Surface>
-        {!indeterminate && showsText ? (
-          <View
-            style={{
-              position: 'absolute',
-              left: textOffset,
-              top: textOffset,
-              width: textSize,
-              height: textSize,
-              borderRadius: textSize / 2,
-              alignItems: 'center',
-              justifyContent: 'center',
-            }}
-          >
-            <Text
-              style={[{
-                color,
-                fontSize: textSize / 4.5,
-                fontWeight: '300',
-              }, textStyle]}
+        {!indeterminate && showsText
+          ? <View
+              style={{
+                position: "absolute",
+                left: textOffset,
+                top: textOffset,
+                width: textSize,
+                height: textSize,
+                borderRadius: textSize / 2,
+                alignItems: "center",
+                justifyContent: "center"
+              }}
             >
-              {formatText(progressValue)}
-            </Text>
-          </View>
-        ) : false}
-        {children}
+              {children
+                ? children
+                : <Text
+                    style={[
+                      {
+                        color,
+                        fontSize: textSize / 4.5,
+                        fontWeight: "300"
+                      },
+                      textStyle
+                    ]}
+                  >
+                    {formatText(progressValue)}
+                  </Text>}
+            </View>
+          : false}
       </View>
     );
   }
